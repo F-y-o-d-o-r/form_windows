1ae32f0279282a1bb7769ca499102dc0ecc6a9c2
Fyodor <pfyodor@gmail.com>
2017-09-30 17:41:56 +0300

    flexbox form + collect info + photo + almost end

--- windows.js
+++ windows.js
@@ -1,5 +1,6 @@
 /**Created by fyodor on 28.09.20 fyodor.work@gmail.com**/
 
 function price() {
-    alert(1)
+    var x = "";
+    document.getElementsByClassName('price')[0].lastElementChild.innerHTML = "<b>Что расчитать: </b>" + window_door.value + "<br><b>Тип профиля: </b>" + profil_type.value + "<br><b>Стеклопакет: </b>" + glass.value + "<br><b>Фурнитура: </b>" + furniture.value + "<br><b>Ширина подоконника: </b>" + width_under.value + "<br><b>Ширина отлива: </b>" + width_2.value + "<br><b>Установка: </b>" + install.value + "<br><b>Ширина: </b>" + width.value + "<br><b>Высота: </b>" + height.value + "<br><b></b>";
 }
